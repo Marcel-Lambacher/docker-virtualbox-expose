8ff8ec0665442ee05097c7f14e631f78e9ed4edb
Marcel Lambacher <marcel.lambacher@zuehlke.com>
2018-08-10 14:20:07 +0200

    Initial project structure created

---
+++ src/DockerVirtualBoxExpose.DockerAgent/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DockerVirtualBoxExpose.DockerAgent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
