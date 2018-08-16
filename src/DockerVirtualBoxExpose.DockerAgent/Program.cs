c50f54cd3cb96bcd73aac508d24b6eb814623188
Marcel Lambacher <web@marcel-lambacher.de>
2018-08-16 18:08:30 +0200

    #14 Add IoC component to DockerAgent
    Merges #15 
    Closes #14

--- src/DockerVirtualBoxExpose.DockerAgent/Program.cs
+++ src/DockerVirtualBoxExpose.DockerAgent/Program.cs
@@ -1,16 +1,36 @@
 ﻿using System;
+using Docker.DotNet;
+using DockerVirtualBoxExpose.Common.Entities;
+using DockerVirtualBoxExpose.DockerAgent.Docker;
+using DockerVirtualBoxExpose.DockerAgent.HostNotification;
+using DockerVirtualBoxExpose.DockerAgent.Watchdog;
+using Microsoft.Extensions.DependencyInjection;
+using NetMQ.Sockets;
 
 namespace DockerVirtualBoxExpose.DockerAgent
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            var service = new DockerAgentService(args);
+            var collection = new ServiceCollection()
+                .AddSingleton<IHostNotificationService>(x =>
+                    new MessageQueueNotificationService(new PushSocket("tcp://localhost:5556")))
+                .AddTransient<IWatcher<ExposedService>, ExposedServiceWatcher>()
+                .AddSingleton<IDockerClient>(x =>
+                    new DockerClientConfiguration(new Uri("unix:///var/run/docker.sock")).CreateClient())
+                .AddTransient<IDockerContainerClient, DockerContainerClient>()
+                .AddTransient<DockerWatchdog>()
+                .AddTransient<DockerAgentService>();
 
-            Console.CancelKeyPress += (sender, eventArgs) => service.Exit();
+            using (var serviceProvider = collection.BuildServiceProvider())
+            {
+                var service = serviceProvider.GetService<DockerAgentService>();
 
-            service.Start();
+                Console.CancelKeyPress += (sender, eventArgs) => service.Exit();
+
+                service.Start();
+            }
         }
     }
 }
