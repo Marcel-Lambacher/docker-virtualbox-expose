5d8d0c14da7812abfa612da7bf6ad060177d47e4
Marcel Lambacher <marcel.lambacher@zuehlke.com>
2018-08-27 14:41:56 +0200

    Added base service structure

--- src/DockerVirtualBoxExpose.HostService/Program.cs
+++ src/DockerVirtualBoxExpose.HostService/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace DockerVirtualBoxExpose.HostService
 {
-    class Program
+    static class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new Service1()
+            };
+            ServiceBase.Run(ServicesToRun);
         }
     }
 }
